b571c7e3d748edee1e32a262e160bd6e560e1650
Isaí Piedra <piedraisai24@gmail.com>
2024-05-15 11:13:03 -0600

    Integración a procesar pedido

--- EC06_Equipo3.py
+++ EC06_Equipo3.py
@@ -1,6 +1,4 @@
 import random
-
-
 """
 Función excFact
 Se invoca con un raise excFact y tira un error
@@ -22,6 +20,47 @@ for i in range(2):
     lista.remove(eliminar)
 diccionario={x:0 for x in lista}
 
+#procesar pedido
+""" 
+Función procesarPedido
+Guarda un pedido en el diccionario de pedidos
+Entradas: color: nombre del color, cantidadGalones: cantidad de galones para hacer el pedido.
+Salidas: N/A
+Restricciones: N/A
+"""
+def procesarPedido (color, cantidadGalones):
+    global diccionario, limite
+    try:
+        color = color.upper().strip() 
+        encontrado = False
+
+        for llaves in diccionario.keys():
+            if llaves.upper().strip() == color:
+                encontrado = True
+                break
+        
+        if not(encontrado):
+            raise excFact(ValueError, "El color no puede ser preparado.")
+    except:
+        raise excFact(ValueError, "El color no puede ser preparado.")
+    
+    try:
+        cantidadGalones = int(cantidadGalones)
+        sumaPedidos = 0
+        for valor in diccionario.values():
+            sumaPedidos += valor
+
+        if cantidadGalones <= 0:
+            raise excFact(ValueError, "El valor no es entero positivo.")
+        elif cantidadGalones >= limite or cantidadGalones >= sumaPedidos:
+            raise excFact(ValueError, "El valor rebasa el máximo permitido de pedidos por semana.")
+        else:
+            for llave, valor in diccionario.items():
+                if llave.upper().strip() == color:
+                    llave = valor + cantidadGalones
+
+    except:
+        raise excFact(ValueError, "El valor no es entero positivo.")
 
 #Menú
 opcion = 1
@@ -39,4 +78,3 @@ while opcion != 0:
             procesarPedido(Color, Cantidad)
         except:
             raise excFact(ValueError,"El valor no es entero positivo.")
-        
