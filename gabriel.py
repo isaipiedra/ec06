863dff3a18a22c3969578d7e375f9bc919f05d1c
Gabriel Ureña <126486842+GabrielOsvaldoSanchez@users.noreply.github.com>
2024-05-15 10:47:56 -0600

    Update gabriel.py función excFact
    función que tira un error

--- gabriel.py
+++ gabriel.py
@@ -1,1 +1,9 @@
-#definir la tupla y eliminar dos colores aleatorios
+"""
+Función excFact
+Se invoca con un raise excFact y tira un error
+Entradas: exception, msg: exception siendo un ValueError, msg siendo string del texto que se mostrará
+Salidas: exception(msg)
+Restricciones: exception, msg : exception siendo un ValueError y msg sinedo tipo str
+"""
+def excFact(exception, msg):
+    return exception(msg)
