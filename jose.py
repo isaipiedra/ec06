635c080d340926b84007dbc28b61b8bfe9a25f4f
JoseAPiedra <164949685+JoseAPiedra@users.noreply.github.com>
2024-05-15 13:19:20 -0600

    Update jose.py

--- jose.py
+++ jose.py
@@ -1,9 +1,15 @@
-#presentar el registro de los pedidos y limpiar el diccionario
+#presentar el registro de los pedidos y limpiar el diccionario; se puede usar cualquiera de las dos opciones, 
+#sin embargo, la segunda no es capaz de limpiar el diccionario
 
-def mostrar_colores_solicitados(diccionario):
+'''def mostrar_colores_solicitados(diccionario):
     diccionario_final = {}
     for x in diccionario: 
         if diccionario[x] > 0:
             diccionario_final[x] = diccionario[x]
     diccionario.clear()
     return diccionario_final
+'''
+def mostrar_colores_solicitados(diccionario):
+    diccionario={x:diccionario[x] for x in diccionario if diccionario[x]>0}
+    return diccionario
+#recordar limpiar el diccionario en la página principal después de llamar a la función
