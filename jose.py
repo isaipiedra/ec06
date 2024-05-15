cd9f5c68607ff6825e54ac1fe94fd5c1d25f0f9a
JoseAPiedra <164949685+JoseAPiedra@users.noreply.github.com>
2024-05-15 11:12:33 -0600

    Update jose.py

--- jose.py
+++ jose.py
@@ -1,1 +1,8 @@
 #presentar el registro de los pedidos y limpiar el diccionario
+
+def mostrar_colores_solicitados(diccionario):
+    diccionario_final = {}
+    for x in diccionario: 
+        if diccionario[x] > 0:
+            diccionario_final[x] = diccionario[x]
+    return diccionario_final
