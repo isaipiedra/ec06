af2f3deae7affedebb2c0e3e95ccc4e4dc4c1d28
Elix1802 <164799782+Elix1802@users.noreply.github.com>
2024-05-15 10:26:12 -0600

    Update EC06_Equipo3.py

---
+++ EC06_Equipo3.py
@@ -0,0 +1,22 @@
+xd=('Tanglewood', 'Opal Basil', 'Vocal Violet', 'Night Image', 'Lilac Murmur', 'Blue Sage', 'Harvest Moon', 
+'Apple Gold', 'Sea Star', 'Lime Slice', 'Garden Room', 'Flamingo', 'Kona Orange', 'Metro Brown', 
+'Cozy Peach', 'April Green', 'Safari Brown') 
+lista=[x for x in xd]
+print(lista)
+for i in range(2):
+    eliminar=random.choice(lista)
+    lista.remove(eliminar)
+print(lista)
+
+diccionario={x:0 for x in lista}
+print(diccionario)
+limite=100
+for x in diccionario:
+    cantidad=int(input(f'Cuantos galones desea para {x}'))
+    cantidad+=cantidad
+    if cantidad>limite:
+        print('ya no se pueden más galones')
+        break
+    diccionario[x]=cantidad
+
+print(diccionario)
